30ee66f2bfc0f977a2484762916480a13ab02831
email <racbernazgul@gmail.com>
2023-08-26 23:03:42 +0300

    header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <Container className="text-center mt-5">
       <Image className="rounded" src={logo} width="200px" />
-      <h1 className="my-2">File'nin SULTANLARI</h1>
+      <h1 className="my-2 text-danger">File'nin SULTANLARI</h1>
     </Container>
   );
 }
--- src/component/PlayerCard.jsx
+++ src/component/PlayerCard.jsx
@@ -5,7 +5,6 @@ import { useState } from "react"
 
 const PlayerCard = ({ name, img, statistics }) => {
   const [showImage, setShowImage] = useState(true);
-  // && || ?:
   return (
     <Col>
       <Card
